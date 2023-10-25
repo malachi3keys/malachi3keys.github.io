055637a7337a9c37683a6386481e6591055b5ece
malachi3keys <74336048+malachi3keys@users.noreply.github.com>
2023-10-25 17:47:24 -0400

    update root colors & create dark mode

--- js/home.js
+++ js/home.js
@@ -14,3 +14,21 @@ const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
 
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+// Dark Mode
+const mode = document.querySelector('#mode');
+const main = document.body;
+mode.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() { 
+  const modeContainer = document.querySelector('.toggle-switch');
+  const dataTheme = main.getAttribute('data-theme');
+ 
+  if(dataTheme === 'dark') {
+    main.setAttribute('data-theme', 'light');
+    modeContainer.setAttribute('title', 'click for dark mode');
+  } else {
+    main.setAttribute('data-theme', 'dark');
+    modeContainer.setAttribute('title', 'click for light mode');
+  }
+ }
